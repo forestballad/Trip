5310e51c2088699608f2d45c140908f86668702f
Anli Ji <anli.azuresand@gmail.com>
2015-02-03 04:45:06 -0500

    Mute/Unmute and Restart level added

---
+++ Assets/Scripts/MuteMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuteMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MuteBGM(){
+		AudioSource theBGM;
+		theBGM = GameObject.Find ("GameMusic").GetComponent<AudioSource> ();
+		if (theBGM.mute == false) {
+			theBGM.mute = true;
+		} else {
+			theBGM.mute = false;
+		}
+	}
+}
---
+++ Assets/Scripts/RestartCurrentLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartCurrentLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RestartLevel(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
